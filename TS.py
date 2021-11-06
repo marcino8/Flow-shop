cca4d7b868e2810ddc5e4fc45ed07af51dc56bd3
marcino8 <mklimczak7@gmail.com>
2021-11-06 12:03:14 +0100

    Added TS DOCS, cleaned up code

--- TS.py
+++ TS.py
@@ -5,46 +5,99 @@ import matplotlib.pyplot as plt
 
 
 def initrandomswap_m(m):
+    """
+    :param m:
+        np matrix obj, to randomly swap its rows
+    :return:
+        np matrix obj, with randomly swapped rows 5000 times
+    """
     m2 = copy.copy(m)
     for i in range(1, 500):
-        m2=randomswap_m(m2)
+        m2 = randomswap_m(m2)
     return m2
 
 
 def calculate_moves2(m, tl):
+    """
+    This is used to calculate every possible swap of 2 rows, and find the best swap
+    :param m:
+        np matrix obj on which all possible swaps of 2 different rows will be made
+    :param tl:
+        np matrix obj, size len(m) x len(m), tabu list with blocked swaps
+    :return:
+        List of 3 ints, first and second are indexes of rows to swap to get best result in time save/loss
+        third is how much time is saved/lost by this swap
+    """
     move = [0, 0, 99999]
-    for i in range(1, len(m)):
+    for i in range(0, len(m)):
         for j in range(i + 1, len(m)):
             if is_not_present_in_tl(tl, i, j):
                 m2 = swap(m, i, j)
                 delta_time = calculate_time_matrices(m2) - calculate_time_matrices(m)
-                if delta_time < move[2]:
+                if delta_time < move[2] and delta_time != 0:
                     move = [i, j, delta_time]
     return move
 
 
 def swap(mt, x, y):
+    """
+    :param mt:
+        np matrix obj, matrix to swap rows
+    :param x:
+        int, row index to swap
+    :param y:
+        int, row index to swap
+    :return:
+        np matrix obj, with swapped x and y rows
+    """
     m = copy.copy(mt)
-    m[[x,y]] = m[[y,x]]
+    m[[x, y]] = m[[y, x]]
     return m
 
 
 def randomswap_m(mt):
+    """
+    :param mt:
+        np matrix obj, matrix to swap 2 random rows
+    :return:
+        np matrix obj, m2 with swapped two random rows
+    """
     m = copy.copy(mt)
     where_to_put, what_to_put = generate_swap_indexes_m(m)
-    m[[where_to_put,what_to_put]] = m[[what_to_put,where_to_put]]
+    m[[where_to_put, what_to_put]] = m[[what_to_put, where_to_put]]
     return m
 
 
 def generate_swap_indexes_m(m):
+    """
+    :param m:
+        obj to get ist lengths
+    :return:
+        List of 2 ints, being 2 random integer numbers from 0 to length of m exclusive
+    """
     return [np.random.randint(0, len(m)), np.random.randint(0, len(m))]
 
 
 def is_not_present_in_tl(tl, i, j):
+    """
+    This method checks if the move about to be made is in tabu list (blocked moves list)
+    :param tl:
+        np matrix obj, tabu list
+    :param i:
+        int, row index to swap
+    :param j:
+        int, row index to swap
+    :return:
+        False if swap is prohibited by Tabu list
+        True otherwise
+    """
     return 0 == tl[i, j] and 0 == tl[j, i]
 
 
 def update_tabu_list(tl):
+    """
+    Updates tabu list after iteration end
+    """
     for i in range(0, len(tl)):
         for j in range(0, len(tl)):
             if tl[i][j] > 0:
@@ -53,10 +106,23 @@ def update_tabu_list(tl):
 
 
 def ts(s, inside_iter, file_to_read, file_to_save):
+    """
+    :param s:
+        int, how many iterations are swaps blocked
+    :param inside_iter:
+        int, how many swaps to make
+    :param file_to_read:
+        string, directory to read from
+    :param file_to_save:
+        string, directory to save to
+    :return:
+        Saves matrix calculated by TS algorithm
+    """
     m = np.genfromtxt(file_to_read, delimiter=',')
     m = m[1:][:]
+    print(calculate_time_matrices(m))
     tabu_list = np.zeros((len(m), len(m)))
-    solution = initrandomswap_m(m)
+    solution = m
     times = []
     for i in range(1, inside_iter):
         start = time.time()
@@ -69,17 +135,22 @@ def ts(s, inside_iter, file_to_read, file_to_save):
         solution = swap(solution, move[0], move[1])
         tabu_list = update_tabu_list(tabu_list)
         tabu_list[move[0], move[1]] = s
-        print("time per inside iter :",time.time() - start)
+        print("time per inside iter :", time.time() - start)
     np.savetxt(file_to_save, solution, delimiter=",")
     print("FINAL FOR ", s, "BLOCKS ", inside_iter, "INSIDE ITERATIONS ", "SAVED TO ", file_to_save)
     plt.plot(times)
     plt.ylabel("czas")
-    plt.show()
-    save = file_to_save+".jpeg"
+    save = file_to_save + ".jpeg"
     plt.savefig(save)
 
-def calculate_time_matrices(matrix):
 
+def calculate_time_matrices(matrix):
+    """
+        :param matrix:
+            np matrix object
+        :return:
+            calculated time for given tasks positioning in flow shop problem
+    """
     m = copy.copy(matrix[:, 1:])
     for row in range(0, len(m)):
         for el in range(0, len(m[0])):
@@ -94,15 +165,4 @@ def calculate_time_matrices(matrix):
     return m[len(m) - 1][len(m[0]) - 1]
 
 
-
-
-
-# ts(5,1000, 'dane2.csv','dane2_ts_2_500.csv')
-ts(5,100, 'dane2.csv','dane2_ts_5_100.csv')
-ts(10,100, 'dane2.csv','dane2_ts_10_100.csv')
-ts(20,100, 'dane2.csv','dane2_ts_20_100.csv')
-ts(5,500, 'dane2.csv','dane2_ts_5_500.csv')
-ts(5,200, 'dane2.csv','dane2_ts_5_200.csv')
-
-
-
+ts(10, 100, 'dane1 tries/dane1_sa.csv', 'dane1ts_10_100.csv')
