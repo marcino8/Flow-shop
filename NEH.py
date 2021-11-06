29dff83b6000a4607a9b907930a2faaa25b803bf
marcino8 <mklimczak7@gmail.com>
2021-11-06 11:37:22 +0100

    NEH rewritten to matrices, added docs and fixed all bugs

--- NEH.py
+++ NEH.py
@@ -1,77 +1,108 @@
-import pandas as pd
+import copy
 import numpy as np
 
 
-def sum_row(df):
+def sum_row(m):
+    """
+    :param m:
+        np matrix object
+    :return:
+        List of sums for every row in given martix
+    """
     sums = []
-    for row in range(len(df.index)):
-        sum = 0
-        for col in range(len(df.columns)):
-            if not isinstance(df.iloc[row, col], str):
-                sum += df.iloc[row, col]
-        sums.append(sum)
+    for row in m:
+        sums.append([sum(row)])
     return sums
 
 
-def load(isOrdered):
-    df = pd.read_csv("dane3.csv")
-    if (isOrdered):
-        df = df.iloc[:, 1:]
-    df['Suma'] = sum_row(df)
-    df = df.sort_values(by='Suma', ascending=False)
-    start(df)
+def load(file_to_read, file_to_save):
+    """
+    :param file_to_read:
+        file to read
+    :param file_to_save:
+        file to save
+    :return:
+        Saves matrix calculated by NEH algorythm
+    """
+    # load matrix
+    matrix = np.genfromtxt(file_to_read, delimiter=',')
+    # transform matrix
+    matrix = matrix[1:][:]
+    matrix = np.append(matrix, sum_row(matrix), axis=1)
+    matrix = matrix[matrix[:, -1].argsort()]
+    matrix = np.flip(matrix, axis=0)
+    # set up 2 first rows
+    first = matrix[0:2, :]
+    second = np.flip(first, axis=0)
+    # position 2 first rows
+    if calculate_time_matrices(first) > calculate_time_matrices(second):
+        start = second
+    else:
+        start = first
+    # start neh
+    for i in range(3, len(matrix)):
+        start = neh(start, matrix[i])
+    np.savetxt(file_to_save, start, delimiter=",")
 
 
-def calculate_time(df):
-    df_calc = df.iloc[:, 1:]
-    df_calc = df_calc.reset_index()
-    df_calc = df_calc.iloc[:, 1:]
-    itr = 0
-    for col in df_calc.columns:
-        for row in df_calc.index:
-            current = df_calc.at[row, col]
-            if row == 0 and itr == 0:
-                df_calc.at[row, col] = current
+def calculate_time_matrices(matrix):
+    """
+    :param matrix:
+        np matrix object
+    :return:
+        calculated time for given tasks positioning in flow shop problem
+    """
+    m = copy.copy(matrix[:, 1:])
+    for row in range(0, len(m)):
+        for el in range(0, len(m[0])):
+            if row == 0 and el == 0:
+                pass
             elif row == 0:
-                df_calc.at[row, col] = current+df_calc.at[row, df_calc.columns[itr - 1]]
-            elif itr == 0:
-                previous_job = df_calc.at[row - 1, col]
-                df_calc.at[row, col] = previous_job + current
+                m[row][el] += m[row, el - 1]
+            elif el == 0:
+                m[row][el] += m[row - 1][el]
             else:
-                previous_job = df_calc.at[row - 1, col]
-                previous_task = df_calc.at[row, df_calc.columns[itr - 1]]
-                df_calc.at[row, col] = max(previous_job, previous_task) + current
-        itr = itr + 1
-    return df_calc.iloc[len(df_calc.index) -1, len(df_calc.columns) - 1]
+                m[row][el] += max(m[row - 1][el], m[row][el - 1])
+    return m[len(m) - 1][len(m[0]) - 2]
 
-def hc(df, new_row):
-    minczas = 99999999
-    pozycja=0
-    df=df.reset_index(drop=True)
-    for i in range(0,len(df.index)+1):
-        df2=insert_row(df,new_row,i)
-        czas=calculate_time(df2)
-        if czas<minczas:
-            minczas=czas
-            pozycja=i;
-    df2=insert_row(df,new_row,pozycja)
+
+def neh(m, new_row):
+    """
+    :param m:
+        np matrix obj, matrix to insert new_row into every possible
+        position and find the best using calculate_time_matrices()
+    :param new_row:
+        np array obj, row to be inserted
+    :return:
+        np matrix obj, with new_row inserted to m, at the best possible position
+    """
+    mintime = 99999999
+    pos = 0
+    for i in range(0, len(m) + 1):
+        df2 = insert_row(m, new_row, i)
+        czas = calculate_time_matrices(df2)
+        if czas < mintime:
+            mintime = czas
+            pos = i
+    df2 = insert_row(m, new_row, pos)
     return df2
 
-def insert_row(df, row, row_index):
-    df.loc[-1]=row
-    df = df.reset_index(drop=True)
-    c=df.loc[row_index]
-    df.loc[row_index]=df.iloc[len(df.index)-1]
-    df.loc[len(df.index) - 1]=c
-    return df
 
-def start(df):
-    odf=df.loc[0:1,:]
-    print(odf)
-    for i in range(1, len(df.index)):
-        odf=hc(odf,df.iloc[i-1,:])
-        print(calculate_time(odf))
-    odf.to_csv("dane2_neh.csv", sep=",")
+def insert_row(m, row, row_index):
+    """
+    :param m:
+        np matrix obj, matrix to insert the row
+    :param row:
+        np array obj, row to be inserted
+    :param row_index:
+        int, index where to insert the row in m
+    :return:
+        np matrix obj, matrix with inserted row to m at position row_index
+    """
+    m = np.insert(m, row_index, row, 0)
+    return m
 
 
-load(True)
+load("dane1.csv", "dane1_neh.csv")
+load("dane2.csv", "dane2_neh.csv")
+load("dane3.csv", "dane3_neh.csv")
